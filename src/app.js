bfb28883be2269b2eb1f9968ec12dd18d036c1fb
Kaushal Krishna <kaushalkrishna011@gmail.com>
2024-11-25 21:29:34 +0530

    added header

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Credentials", "true");
     res.header(
       "Access-Control-Allow-Headers",
-      "Content-Type, Authorization"
+      "Content-Type, Authorization, fiyoat, fiyort"
     );
     res.header(
       "Access-Control-Allow-Methods",
