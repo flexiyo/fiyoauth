84b0bd48a6bce55c4c27cf4a305bdee7cf0cc859
Kaushal Krishna <kaushalkrishna011@gmail.com>
2024-12-10 18:57:21 +0530

    added http://localhost

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ app.use(cookieParser());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
+
 /** List of allowed origins */
 const allowedOrigins = ["http://localhost", "http://localhost:3000", "https://flexiyo.web.app"];
 
@@ -18,7 +19,9 @@ const allowedOrigins = ["http://localhost", "http://localhost:3000", "https://fl
 app.use((req, res, next) => {
   const origin = req.get("Origin");
   const isApiRoute = req.path.startsWith("/api/v1");
-
+  
+  console.log(origin)
+  
   if (isApiRoute && allowedOrigins.includes(origin)) {
     res.header("Access-Control-Allow-Origin", origin);
     res.header("Access-Control-Allow-Credentials", "true");
