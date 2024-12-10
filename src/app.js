a72551915a021b90c7a1dd00182fd19223a65103
Kaushal Krishna <kaushalkrishna011@gmail.com>
2024-12-10 18:53:15 +0530

    added http://localhost

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
 /** List of allowed origins */
-const allowedOrigins = ["http://localhost:3000", "https://flexiyo.web.app"];
+const allowedOrigins = ["http://localhost", "http://localhost:3000", "https://flexiyo.web.app"];
 
 /** CORS Middleware */
 app.use((req, res, next) => {
